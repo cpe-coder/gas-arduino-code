5a24a4f4e1dedf1b13d216bdec4020babb47ed91
Grian Gajila <griangajila2@gmail.com>
2025-04-29 23:09:20 +0800

    first commit

---
+++ gas-arduino-code.ino
@@ -0,0 +1,96 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include <Servo.h>
+
+#define mainBulb D0
+#define reservedBulb D1
+
+#define WIFI_SSID "G-Coder"
+#define WIFI_PASSWORD "G-Coder@02"
+
+#define API_KEY "AIzaSyBNZ1aVpWALpintHstXLPC9uyC992zzRfw"
+#define DATABASE_URL "gas-monitoring-a2b73-default-rtdb.firebaseio.com/"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool signupOK = false;
+int boilSize = 0;
+int transferSize = 0;
+
+
+unsigned long sendDataPrevMillis = 0;
+
+void setup() {
+  pinMode(mainBulb, OUTPUT);
+  pinMode(reservedBulb, OUTPUT);
+  digitalWrite(mainBulb, HIGH);
+  digitalWrite(reservedBulb, HIGH);
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Firebase connection successful");
+    signupOK = true;
+  } else {
+    Serial.printf("Firebase sign-up error: %s\n", config.signer.signupError.message.c_str());
+  }
+
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+
+void loop() {
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 500 || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
+
+    if (Firebase.RTDB.getBool(&fbdo, "controls/mainBulb")) {
+      if (fbdo.dataType() == "boolean"){
+      bool mainBulbStateStr = fbdo.boolData();
+      Serial.println("Seccess: " + fbdo.dataPath() + ": " + mainBulbStateStr + "(" + fbdo.dataType() + ")");
+      bool mainBulbState = (mainBulbStateStr == false) ? HIGH : LOW;
+      digitalWrite(mainBulb, mainBulbState);   
+      }
+    } else {
+      Serial.println("Failed to read Auto: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.getBool(&fbdo, "controls/reservedBulb")) {
+      if (fbdo.dataType() == "boolean"){
+      bool reservedBulbStateStr = fbdo.boolData();
+      Serial.println("Seccess: " + fbdo.dataPath() + ": " + reservedBulbStateStr + "(" + fbdo.dataType() + ")");
+      bool reservedBulbState = (reservedBulbStateStr == false) ? HIGH : LOW;
+      digitalWrite(reservedBulb, reservedBulbState);   
+      }
+    } else {
+      Serial.println("Failed to read Auto: " + fbdo.errorReason());
+    }
+
+  
+
+    Serial.println("_______________________________________");
+  }
+}
+
+
+
+
